7032c0ef0d5b6c45786b11454b35e288db195082
sasano8 <y-sasahara@ys-method.com>
2026-01-04 07:59:00 +0900

    update

--- internal/core/repository/repository_test.go
+++ internal/core/repository/repository_test.go
@@ -3,51 +3,53 @@ package repository
 import (
 	"testing"
 
-	"github.com/sasano8/kvtool/internal/core/testutil"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRepository(t *testing.T) {
 	var v any
 	var err error
 
-	t2 := testutil.New(t)
+	// r と assert がある。
+	// r は即時中断、assert は bool を返す。
+	r := require.New(t)
 
 	reg := New[int]()
 
 	// 要素数が0であること
-	t2.AssertEqual(0, len(reg))
+	r.Equal(0, len(reg))
 
 	v, err = reg.Create("key1", 1)
-	t2.AssertEqual(1, len(reg))
-	t2.IsNil(err)
-	t2.AssertEqual(1, v)
+	r.Equal(1, len(reg))
+	r.Nil(err)
+	r.Equal(1, v)
 
 	v, err = reg.Get("key1")
-	t2.AssertEqual(1, v)
+	r.Equal(1, v)
 
 	v, err = reg.Create("key1", 2)
-	t2.AssertEqual(1, len(reg))
-	t2.IsError(err)
+	r.Equal(1, len(reg))
+	r.Error(err)
 
 	// 値が変わっていないこと
 	v, err = reg.Get("key1")
-	t2.AssertEqual(1, v)
+	r.Equal(1, v)
 
 	err = reg.Delete("key1")
-	t2.AssertEqual(0, len(reg))
-	t2.IsNil(err)
+	r.Equal(0, len(reg))
+	r.Nil(err)
 	v, err = reg.Get("key1")
-	t2.IsError(err)
+	r.Error(err)
 
 	err = reg.Delete("key1")
-	t2.AssertEqual(0, len(reg))
-	t2.IsError(err)
+	r.Equal(0, len(reg))
+	r.Error(err)
 
 	v, err = reg.Put("key1", 1)
 	v, err = reg.Get("key1")
-	t2.AssertEqual(1, v)
+	r.Equal(1, v)
 
 	v, err = reg.Put("key1", 2)
 	v, err = reg.Get("key1")
-	t2.AssertEqual(2, v)
+	r.Equal(2, v)
 }
